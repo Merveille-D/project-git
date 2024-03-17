924888818b634dff77f9e4c05e13937cd2723f42
Merveille Dohou <merveilledohou989@gmail.com>
2024-03-17 22:34:02 +0000

    nouveaux fichieré

---
+++ diviseur-propre.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   	//int nbr, a;
+
+	printf("Entrez votre nombre\n");
+/*	scanf("%d",&nbr);
+
+	if((nbr == 1) || (nbr == 0))
+	 printf("Pas de diviseur propre");
+
+	else{
+		printf("les diviseurs propre de %d sont: ",nbr);
+        a = 1;
+		while(a<nbr)
+		{
+          if(nbr % a == 0 )
+           {
+		      printf("%d " , a);
+            }
+			a++;
+		}
+	}
+
+	int ent,cal=0,resul=0;
+
+	printf("entrez votre nombre ");
+	scanf("%d",&ent);
+	while(cal<ent)
+    {
+       resul=resul+1;
+       cal++;
+    }
+    printf("la somme des nombres entrées est %d:",resul);
+
+    int nomb,result,i;
+    printf("Entrez un nombre: ");
+
+    scanf("%d", &nomb);
+
+	while(i < nomb)
+    {
+       if((i % 2) == 0)
+       {
+           result += i;
+       }
+       i++;
+    }
+     printf("Somme des nombres pairs %d: ",result);
+ */
+
+    return 0;
+}
